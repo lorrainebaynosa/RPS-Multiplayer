e4385910711d80644014e28d45a6024fec2af4f3
LORRAINE BAYNOSA <lorrainebaynosa@LORRAINEs-MacBook-Pro.local>
2019-04-28 12:31:24 -0700

    just add Review of Concepts Zip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# RPS-Multiplayer
+# Train-Scheduler
+The Train-Scheduler interface allows the user to determine Current Train Schedules based on user input (Train Name, Destination, First Train Time (HH:mm - military time), and Frequency (min)) and the current time.
+To generate the list of Current Train Schedules, fill out all user input text boxes at the bottom of the page and click on the Submit button. Watch as each user input submission translates onto the webpage with respect to Train Name, Destination, Frequency (min), Next Arrival, and Minutes Away through synchronization with Google's Firebase Realtime Database.
+Lorraine Baynosa maintains and contributes to the project.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 // create train schedule application at that incorporates Firebase to host arrival and departure data.
 // users from many different machines must be able to view same train times.
 // FIREBASE
-    // initiate Firebase with your unique var config
-    // create reference to Firebase
+// initiate Firebase with your unique var config
+// create reference to Firebase
 // WEBPAGE DISPLAY
 
 var config = {
@@ -12,71 +12,71 @@ var config = {
     projectId: "fir-ucla-bootcamp",
     storageBucket: "fir-ucla-bootcamp.appspot.com",
     messagingSenderId: "79682732387"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  // Create a variable to reference the database
-  var database = firebase.database();
+// Create a variable to reference the database
+var database = firebase.database();
 
 // WEBPAGE DISPLAY
 // CAPTURE CLICK OF SUBMIT BUTTON - BUTTON FOR ADDING TRAINS
 // event.preventDefault(); to prevent early submission via single character entry
 // $("#add-train-btn") is ID for submit button
 
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
-// CAPTURE value of user input for Train Name, Destination, First Train Time, and Frequency (min) using jQuery.val() method. jQuery.trim() method to remove extra spaces
+    // CAPTURE value of user input for Train Name, Destination, First Train Time, and Frequency (min) using jQuery.val() method. jQuery.trim() method to remove extra spaces
 
-var trainName = $("#train-name-input").val().trim();
-var destination = $("#destination-input").val().trim();
-var firstTrainTime = $("#first-train-time-input").val().trim();
-//  use moment.js to get military time
-var frequency = $("#frequency-input").val().trim();
-// CLEAR VALUE OF INPUT TEXT BOXES AFTER SUBMISSION using jQuery .val("") method
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrainTime = $("#first-train-time-input").val().trim();
+    //  use moment.js to get military time
+    var frequency = $("#frequency-input").val().trim();
+    // CLEAR VALUE OF INPUT TEXT BOXES AFTER SUBMISSION using jQuery .val("") method
 
-$("#train-name-input").val("");
-$("#destination-input").val("");
-$("#first-train-time-input").val("");
-$("#frequency-input").val("");
-// METHOD FOR CODE FOR WEBPAGE DISPLAY TO SYNCHRONIZE WITH FIREBASE DATABASE: CREATE OBJECT TO PUSH TO FIREBASE
-// FIREBASE: assign object key and value pairs for pushing to FIREBASE; eg: key is name: and value is trainName; values for keys in Firebase equal to value variable used in webpage display
-var newTrain = {
-    name: trainName,
-    endpoint: destination,
-    time: firstTrainTime,
-    often: frequency
-}
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time-input").val("");
+    $("#frequency-input").val("");
+    // METHOD FOR CODE FOR WEBPAGE DISPLAY TO SYNCHRONIZE WITH FIREBASE DATABASE: CREATE OBJECT TO PUSH TO FIREBASE
+    // FIREBASE: assign object key and value pairs for pushing to FIREBASE; eg: key is name: and value is trainName; values for keys in Firebase equal to value variable used in webpage display
+    var newTrain = {
+        name: trainName,
+        endpoint: destination,
+        time: firstTrainTime,
+        often: frequency
+    }
 
-// Transfers/pushes data from newTrain object to FIREBASE REALTIME DATABASE
-// newTrain object is pushed to ROOT of FIREBASE database
-// Ways to Save Data
-// set:	Write or replace data to a defined path, like messages/users/<username>
-// update:vUpdate some of the keys for a defined path without replacing all of the data
-// push: Add to a list of data in the database. Every time you push a new node onto a list, your database generates a unique key, like messages/users/<unique-user-id>/<username>
-// transaction: Use transactions when working with complex data that could be corrupted by concurrent updates
+    // Transfers/pushes data from newTrain object to FIREBASE REALTIME DATABASE
+    // newTrain object is pushed to ROOT of FIREBASE database
+    // Ways to Save Data
+    // set:	Write or replace data to a defined path, like messages/users/<username>
+    // update:vUpdate some of the keys for a defined path without replacing all of the data
+    // push: Add to a list of data in the database. Every time you push a new node onto a list, your database generates a unique key, like messages/users/<unique-user-id>/<username>
+    // transaction: Use transactions when working with complex data that could be corrupted by concurrent updates
     database.ref().push(newTrain);
 
-// Verify pushing correct data to FIREBASE
-// REVIEW OF CONCEPTS (below): console for Firebase Database - not for webpage console
-console.log(newTrain.name);
-console.log(newTrain.endpoint);
-console.log(newTrain.time);
-console.log(newTrain.often);
+    // Verify pushing correct data to FIREBASE
+    // REVIEW OF CONCEPTS (below): console for Firebase Database - not for webpage console
+    console.log(newTrain.name);
+    console.log(newTrain.endpoint);
+    console.log(newTrain.time);
+    console.log(newTrain.often);
 
-alert("Train successfully added.");
+    alert("Train successfully added.");
 });
 
 // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
 
 // REVIEW OF CONCEPTS, FIREBASE (below):
-    // Firebase database is a JSON tree.
-    // To retrieve data, think in terms of dataSnapshot: pick a key, where key would be a dataSnapshot, and value for key would be everything downstream of the key. EVERY KEY has a dataSnapshot.
-    
-    // DataSnapshot contains data from a Database location. Any time you read data from the Database, you receive the data as a dataSnapshot. A dataSnapshot is passed to the event callbacks you attach with on() or once(). You can extract the contents of the snapshot as a JavaScript object by calling the val() method. Or you can look into the snapshot by calling child()to return child snapshots (which you could then call val() on).
-        
-    // on() method: on(eventType: EventType, callback: function)
-    // Listens for data changes at a particular location.
+// Firebase database is a JSON tree.
+// To retrieve data, think in terms of dataSnapshot: pick a key, where key would be a dataSnapshot, and value for key would be everything downstream of the key. EVERY KEY has a dataSnapshot.
+
+// DataSnapshot contains data from a Database location. Any time you read data from the Database, you receive the data as a dataSnapshot. A dataSnapshot is passed to the event callbacks you attach with on() or once(). You can extract the contents of the snapshot as a JavaScript object by calling the val() method. Or you can look into the snapshot by calling child()to return child snapshots (which you could then call val() on).
+
+// on() method: on(eventType: EventType, callback: function)
+// Listens for data changes at a particular location.
 
 // This is the primary way to read data from a Database. Your callback will be triggered for the initial data and again whenever the data changes. 
 
@@ -100,88 +100,65 @@ alert("Train successfully added.");
 //   ...
 // });
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
     console.log(childSnapshot.val());
-  
+
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
     var destination = childSnapshot.val().endpoint;
     var firstTrainTime = childSnapshot.val().time;
     var frequency = childSnapshot.val().often;
-  
+
     // Employee Info
     console.log(trainName);
     console.log(destination);
     console.log("first train snapshot " + firstTrainTime);
     console.log(frequency);
-  
-var frequency = childSnapshot.val().often;
-var firstTrainTime = childSnapshot.val().time;
 
-// First Time (pushed back 1 year to make sure it comes before current time)
-var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-console.log(firstTrainTimeConverted);
+    var frequency = childSnapshot.val().often;
+    var firstTrainTime = childSnapshot.val().time;
 
-// Current Time
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+    console.log(firstTrainTimeConverted);
 
-// Difference between the times
-var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-// Time apart (remainder)
-var tRemainder = diffTime % frequency;
-console.log(tRemainder);
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-// Minute Until Train
-var tMinutesTillTrain = frequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
 
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // Minutes Until Next Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // // Difference between the times
-    // var diffTime = moment().diff(trainTConver, "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % frequency;
-    // console.log("remainder " + tRemainder);
-
-    // // Minute Until Train
-    // var tMinutesTillTrain = frequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
-    // console.log("ARRIVAL TIME: " + nextTrain);
-
-    // Create the new row
     var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destination),
-      $("<td>").text(frequency),
-      $("<td>").text(nextTrain),
-      $("<td>").text(tMinutesTillTrain)
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain)
     );
-  
+
     // Append the new row to the table
     $("#train-table > tbody").append(newRow);
 
-  });
+});
 
 
 
 
 
-  
-    
-  
-    
+
+
+
